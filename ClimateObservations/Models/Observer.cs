3a72cd3d8441e68567b4cbff2800e5dcf85a5a6d
Olivia Björner <filipokus.130@hotmail.com>
2020-03-21 13:16:08 +0100

    Add project files.

---
+++ ClimateObservations/Models/Observer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClimateObservations.Models
+{
+    class Observer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ ClimateObservations/Repositories/ObserverRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace ClimateObservations.Repositories
+{
+    class ObserverRepository
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["dbLocal"].ConnectionString;
+        #region CREATE
+
+        #endregion
+        #region READ
+
+        #endregion
+        #region UPDATE
+
+        #endregion
+        #region DELETE
+
+        #endregion
+    }
+}
