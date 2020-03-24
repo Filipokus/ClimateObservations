b0958cae4a6236657a22f6fbf472f5862ece50f8
Olivia Björner <filipokus.130@hotmail.com>
2020-03-24 18:59:24 +0100

    Measurement class

---
+++ ClimateObservations/Models/Measurement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClimateObservations.Models
+{
+    class Measurement
+    {
+        public int Id { get; set; }
+        public double Value { get; set; }
+        public int ObservationId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
